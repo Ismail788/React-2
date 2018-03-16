d4c977d5743f23e5a7c02810a187255a5b020e8a
Ismail788 <ismail.soliman788@gmail.com>
2018-03-16 21:11:08 +0100

    Public index

---
+++ footer.js
@@ -0,0 +1,15 @@
+/*import React, { Component } from 'react';
+
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+     <p>CopyRight by Ismail</p>
+   </div>
+
+);
+  }
+}
+
+export default Footer;*/
